f25df6bf21ae52d280956c519803c839f55db344
Moshe Brevda <mbrevda@gmail.com>
2015-07-17 10:12:54 +0300

    Initial commit

---
+++ src/SpecificationQueryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mbrevda\SpecificationQueryInterface;
+
+interface SpecificationQueryInterface
+{
+    public function andX();
+    public function orX();
+    public function not();
+    
+    public function equals();
+    public function inequals();
+}
